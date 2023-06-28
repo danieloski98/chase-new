738ae0edf3fe4ed72a0fe23c4a1c135211264f4e
ishaya-chasescroll <ishaya.solomon@chasescroll.com>
2023-06-28 15:57:43 +0100

    attempt to fix issue with production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,3 +12,5 @@ export default defineConfig({
     }
   }
 })
+
+dotenv.config();
