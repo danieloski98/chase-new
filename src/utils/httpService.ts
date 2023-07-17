ec2935ce52f674de1e717e6810f7ce4c7132992a
daniel <danielemmanuel257@gmail.com>
2023-07-17 09:13:49 +0100

    transaction and fixes

---
+++ src/utils/httpService.ts
@@ -0,0 +1,54 @@
+import axios, { AxiosError } from 'axios';
+
+const httpService = axios.create({
+    baseURL: `${import.meta.env.VITE_BASE_URL}`,
+});
+
+httpService.interceptors.request.use(async(config) => {
+    const token = localStorage.getItem('token')
+    if (config.data instanceof FormData) {
+        console.log('ITS FORMDATA');
+        config.headers['Content-Type'] = 'multipart/form-data'
+        if (token === null) {
+            return config;
+        }
+        config.headers!['authorization'] = `Bearer ${token}`;
+    
+        return config;
+    } else {
+        config.headers!['Content-Type'] = 'application/json';
+        if (token === null) {
+            return config;
+        }
+        config.headers!['authorization'] = `Bearer ${token}`;
+    
+        return config;
+    }
+    
+   
+},  error => {
+    return Promise.reject(error)
+}); 
+
+httpService.interceptors.response.use((data) => {
+    return data;
+}, async(error: AxiosError<any, any>) => {
+    console.log(error.message);
+    // return Promise.reject('An Error occurred');
+    if (!error.response) {
+        return Promise.reject(error.message);
+    } else {
+        if (error.response?.data.message instanceof Array) {
+            const msg = error.response?.data.message as Array<any>;        
+            return Promise.reject(JSON.stringify(error.response?.data.message));
+        } else {
+            if (error.response.status === 401 || error.response.status === 403) {
+             localStorage.setItem('token', '');
+            }
+            
+            return Promise.reject(error.response?.data.message);
+        }
+    }
+});
+
+export default httpService;
